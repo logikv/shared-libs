62245a4c3b1b80d1ec7327eac5e9aed81215cb70
logikv <logikv@yandex.ru>
2024-10-04 21:43:41 +0300

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,7 @@
+import org.example.AddSidebarLinkAction;
+
+def cl = new AddSidebarLinkAction()
+
 def call(Map config = [:]){
-  echo "Hello ${config.name}"
+  echo "Hello ${config.name} ${cl.num} "
 }
