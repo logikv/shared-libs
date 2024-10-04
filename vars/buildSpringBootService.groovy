253936f9cae0785db111490b0edb28031e538bec
logikv <logikv@yandex.ru>
2024-10-04 21:05:32 +0300

    Create buildSpringBootService.groovy

---
+++ vars/buildSpringBootService.groovy
@@ -0,0 +1,39 @@
+def call(Map config = [:]){
+pipeline {
+       agent any
+       tools {
+           maven 'M3'
+           jdk "${config.jdkVersion}"
+       }
+       stages {
+           stage("Tools initialization") {
+               steps {
+                   sh "mvn --version"
+                   sh "java -version"
+               }
+           }
+           stage("Checkout Code") {
+               steps {
+                   git branch: 'master',
+                       url: "${config.gitUrl}"
+               }
+           }
+           stage("Cleaning workspace") {
+               steps {
+                   sh "mvn clean"
+               }
+           }
+           stage("Running Testcase") {
+              steps {
+                   sh "mvn test"
+               }
+           }
+           stage("Packing Application") {
+               steps {
+                   sh "mvn package -DskipTests"
+               }
+           }
+       }
+ }
+
+}
