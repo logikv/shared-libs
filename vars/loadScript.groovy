5aab89d97c8992cf0446059bd791b7705166b8c1
logikv <logikv@yandex.ru>
2024-10-04 19:09:56 +0300

    Delete vars/loadScript.groovy

--- vars/loadScript.groovy
+++
@@ -1,6 +0,0 @@
-def call(Map config = [:]) {
-  def resourceContent = libraryResource("com/example/${config.name}")
-  writeFile file: "${config.name}", text: resourceContent
-  sh "chmod u+x ./${config.name}"
-  sh "chmod 777 ./"
-}
